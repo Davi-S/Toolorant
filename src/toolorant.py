5a1ceab27c5fe2cc87584c4d2c9b4e5859a7e057
Davi S <davialvessampaio00@gmail.com>
2023-10-05 20:46:11 -0300

    Added minimum size to pywebview

--- src/toolorant.py
+++ src/toolorant.py
@@ -15,7 +15,7 @@ print('===== Finished imports =====')
 
 print('===== Starting main code =====')
 flask_app = app.create_app()
-webview.create_window('Toolorant', flask_app, width=1200, height=650)
+webview.create_window('Toolorant', flask_app, width=1200, height=650, min_size=(1050, 650))
 webview.start()
 print('===== Finished main code =====')
 print('===== Exiting "toolorant.py" file =====')
