208882da8af10868834c3b39ca9e99a647002b74
Davi S <davialvessampaio00@gmail.com>
2023-11-01 17:47:25 -0300

    Added stream hunter class

--- src/pages/stream_hunter/streamhunterpageqwidget.py
+++ src/pages/stream_hunter/streamhunterpageqwidget.py
@@ -4,8 +4,10 @@ import PySide6.QtCore as QtCore
 import PySide6.QtGui as QtGui
 import PySide6.QtWidgets as QtWidgets
 
+import mainwindowqmainwindow
 import page_manager
 
+from .stream_hunter import StreamHunter
 from .view.stream_hunter_pg_ui import Ui_stream_hunter_pg
 
 logger = logging.getLogger(__name__)
@@ -15,6 +17,9 @@ class StreamHunterPageQWidget(page_manager.BasePageQWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setup_ui()
+        self.stream_hunter = StreamHunter(
+            client=mainwindowqmainwindow.get_main_window().client
+        )
         
         self.ui.menu_btn.clicked.connect(lambda: self.page_manager.switch_to_page('main_menu_pg'))
         self.ui.hunt_btn.clicked.connect(self.hunt_btn_clicked)
